6420a268b7a48beb28f02c93a83ae7d1661f7ddc
Karol Piekarski <108413343+karolzblueowl@users.noreply.github.com>
2023-01-10 21:22:42 +0100

    Add translate

--- src/Generator/Repository.php
+++ src/Generator/Repository.php
@@ -2,7 +2,48 @@
 
 namespace PiekarskiK\LaravelCrudGenerator\Generator;
 
-class Repository
+use PiekarskiK\LaravelCrudGenerator\Helpers\Helper;
+
+class Repository extends Helper
 {
+    private string $name;
+    private string $path;
+    private string $variant;
     
+    public function __construct(string $name, string $path)
+    {
+        $this->name = $name;
+        $this->path = $path;
+    }
+    
+    public function create()
+    {
+        $className = $this->name.'Repository';
+        $basePath  = app_path().str_replace('app', '', $this->path);
+        $this->checkAndCreateDir($basePath);
+        
+        $template = $this->createTemplate(
+            [
+                '{{className}}',
+                '{{name}}',
+                '{{nameLower}}',
+                '{{model}}',
+                '{{modelLower}}',
+                '{{namespace}}',
+            ],
+            [
+                $className,
+                $this->name,
+                strtolower($this->name),
+                $this->name,
+                strtolower($this->name),
+                config('crud.repositories.namespace')
+            ],
+            "repository"
+        );
+        
+        echo $this->path . "Repository.php";
+        
+        file_put_contents($this->path . DIRECTORY_SEPARATOR . $className . ".php", $template);
+    }
 }
--- src/Helpers/Helper.php
+++ src/Helpers/Helper.php
@@ -6,11 +6,18 @@ class Helper
 {
     protected function getStub($name): bool|string
     {
-        return file_get_contents(__DIR__ . "/stubs/{$name}.stub");
+        return file_get_contents(__DIR__."/../stubs/{$name}.stub");
     }
     
     protected function createTemplate(array $find, array $replace, string $stubName): string
     {
         return str_replace($find, $replace, $this->getStub($stubName));
+    }
+    
+    public function checkAndCreateDir($path)
+    {
+        if ( ! is_dir($path)) {
+            mkdir($path, 0755, true);
+        }
     }
 }
